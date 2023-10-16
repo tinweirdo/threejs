31547db53b8a476ee3d103cd0610921c89423b0c
tinweirdo <tinweirdo@gmail.com>
2023-10-16 23:11:23 +0800

    fix bug

--- vite-project/src/spotLight-and-shadow.js
+++ vite-project/src/spotLight-and-shadow.js
@@ -8,7 +8,6 @@ import gui from './gui.js';
 renderer.shadowMap.enabled = true;
 
 // 3.设置物体投射阴影 sphere.castShadow = true
-// FIXME-t 为什么阴影很小
 const sphereGeometry = new THREE.SphereGeometry(1);
 const sphereMaterial = new THREE.MeshStandardMaterial();//默认就是白色
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
@@ -19,21 +18,29 @@ scene.add(sphere);
 // 4.设置光照投射阴影 directionLight.catShadow = true
 // 灯光
 // 环境观
-const light = new THREE.AmbientLight(0xffffff,1)
+const light = new THREE.AmbientLight(0xffffff, 1)
 scene.add(light)
 // 从上方照射的白色平行光，强度为 0.5。
-const spotLight = new THREE.SpotLight(0xffffff,1);
-spotLight.position.set(5, 5, 5);
+const spotLight = new THREE.SpotLight(0xffffff, 200);
+spotLight.position.set(10, 10, 10);
 // 设置阴影贴图模糊度
 spotLight.shadow.radius = 20;//模糊阴影的边缘像素值（px），继承 LightShadow
 // 设置阴影贴图分辨率
-spotLight.shadow.mapSize.set(2048, 2048);//设置大一些，阴影更加细致，默认 512 x 512
+spotLight.shadow.mapSize.set(5000, 5000);//设置大一些，阴影更加细致，默认 512 x 512
 // 设置透视相机属性
 spotLight.castShadow = true//设置光照投射阴影
 
 // 聚光灯跟踪物体
 spotLight.target = sphere;
+// 光线角度范围
+spotLight.angle = Math.PI / 6;
 scene.add(spotLight);
+gui.add(sphere.position, "x").min(-5).max(5).step(0.1).name("球体 x 坐标")
+gui.add(spotLight, "angle").min(0).max(Math.PI / 2).step(0.1).name("光线角度范围")
+gui.add(spotLight, "distance").min(0).max(50).step(0.1).name("光线距离")
+gui.add(spotLight, "penumbra").min(0).max(1).step(0.1).name("阴影衰减百分比")
+gui.add(spotLight, "decay").min(0).max(5).step(0.1).name("光线衰减百分比")//需要在物理模式下
+
 
 // 5.设置物体接受阴影 plane.receiveShadow = true
 const planeGeometry = new THREE.PlaneGeometry(50, 50);
